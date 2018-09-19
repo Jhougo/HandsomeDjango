615ade2ca329fca5a1f03be796c061f7022fb0a5
Jeffrey <jhougo7928@gmail.com>
2018-09-19 09:54:53 +0800

    test

--- Handsome_site/settings.py
+++ Handsome_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_d_8*i7_zb3v6neshlw((a77d!uvo#vi9i^m32yvs%!qy92pnu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
