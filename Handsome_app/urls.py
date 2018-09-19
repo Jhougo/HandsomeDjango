fa490a021cfc0385978b439eb1b5e394cc7907b4
Jeffrey <jhougo7928@gmail.com>
2018-09-19 11:32:48 +0800

    test

--- Handsome_app/urls.py
+++ Handsome_app/urls.py
@@ -2,6 +2,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('1', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('2', views.hello_world, name='hello_world'),
 ]
--- Handsome_site/urls.py
+++ Handsome_site/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('polls/', include('Handsome_app.urls')),
+    path('', include('Handsome_app.urls')),
     path('admin/', admin.site.urls),
-    path('hello/', include('Handsome_app.urls')),
 ]
